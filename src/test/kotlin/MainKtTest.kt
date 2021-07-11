414f5195907e86cefd5db60a045f897496381b57
Adabe Echaim <adabeechaim@gmail.com>
2021-07-12 00:26:01 +0100

    added question and tests

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -11,45 +11,45 @@ class MainKtTest {
     }
 
     @Test
-    fun `empty string returns empty`() {
-        val result = reverseString("")
-        assertEquals(result.isEmpty(), true )
+    fun `empty array returns 0`(){
+        val result = sumOfSquares(arrayListOf())
+        assertEquals(result, 0)
     }
 
     @Test
-    fun `Good food returns doof dooG`(){
-        val result = reverseString("Good food")
-        assertEquals(result, "doof dooG")
+    fun `1, 2, 2, returns 9`(){
+        val result = sumOfSquares(arrayListOf(1, 2, 2))
+        assertEquals(result, 9)
     }
 
     @Test
-    fun `real test returns tset laer`(){
-        val result = reverseString("real test")
-        assertEquals(result, "tset laer")
+    fun `1, 2, 3 returns 14`(){
+        val result = sumOfSquares(arrayListOf(1, 2, 3))
+        assertEquals(result, 14)
     }
 
     @Test
-    fun `long string works correctly`(){
-        val testString = "THE brown fox jumps over the lazy dog."
-        val result = reverseString(testString)
-        val expected = testString.reversed()
-        assertEquals(result, expected)
-    }
-
-
-    @Test
-    fun `special characters works correctly`(){
-        val testString = "$#%^@!.,;:}{[]."
-        val result = reverseString(testString)
-        val expected = testString.reversed()
-        assertEquals(result, expected)
+    fun `3, 6, 9 returns 126`(){
+        val result = sumOfSquares(arrayListOf(3, 6, 9))
+        assertEquals(result, 126)
     }
 
     @Test
-    fun `string of numbers works correctly`(){
-        val testString = "1234567890908754632"
-        val result = reverseString(testString)
-        val expected = testString.reversed()
-        assertEquals(result, expected)
+    fun `10, 12, 12 returns 388`(){
+        val result = sumOfSquares(arrayListOf(10, 12, 12))
+        assertEquals(result, 388)
     }
+
+    @Test
+    fun `2, 2, 3, 3, 4, 5, 6 returns 103`(){
+        val result = sumOfSquares(arrayListOf(2, 2, 3, 3, 4, 5, 6))
+        assertEquals(result, 103)
+    }
+
+    @Test
+    fun `25, 30, 49, 100 returns 13926`(){
+        val result = sumOfSquares(arrayListOf(25, 30, 49, 100))
+        assertEquals(result, 13926)
+    }
+
 }
