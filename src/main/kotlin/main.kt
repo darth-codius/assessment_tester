5224ce1a495a1bb482ee9948345a393e775adad5
Adabe Echaim <adabeechaim@gmail.com>
2021-07-12 00:26:37 +0100

    code refactor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,3 @@
-fun main() {
-    println(sumOfSquares(arrayListOf(10, 12, 12)))
-}
-
 //Complete the sumOfSquares function so that it squares each number passed into it and then sums the results together.
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
